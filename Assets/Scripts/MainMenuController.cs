2e9132f4892c55efa2744651df49009be1dda33a
AnouarWalziki <82971155+AnouarWalziki@users.noreply.github.com>
2022-05-14 17:55:36 +0200

    MainMenu : added option to choose level to play

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,6 +7,8 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject MainMenuPage;
     public GameObject HowToPlayMenuPage;
+    public GameObject ChooseStartPage;
+    public GameObject ChooseLevelPage;
 
     public GameObject sceneLoader;
 
@@ -14,30 +16,68 @@ public class MainMenuController : MonoBehaviour
     {
         MainMenuPage.SetActive(true);
         HowToPlayMenuPage.SetActive(false);
+        HowToPlayMenuPage.SetActive(false);
+        ChooseStartPage.SetActive(false);
+        ChooseLevelPage.SetActive(false);
+
+        Cursor.visible = true;
+        Screen.lockCursor = false;
     }
 
-    public void Play()
+    public void NewGame()
     {
         sceneLoader.GetComponent<SceneLoader>().sceneToLoad = "StartingLevel";
         sceneLoader.GetComponent<SceneLoader>().StartLoad();
     }
 
-    public void LoadMainMenuScene()
+    public void LoadLevel1()
     {
-        sceneLoader.GetComponent<SceneLoader>().sceneToLoad = "MainMenu";
+        sceneLoader.GetComponent<SceneLoader>().sceneToLoad = "Room1";
         sceneLoader.GetComponent<SceneLoader>().StartLoad();
     }
 
-    public void OpenHelpMenu()
+    public void LoadLevel2()
     {
-        MainMenuPage.SetActive(false);
-        HowToPlayMenuPage.SetActive(true);
+        sceneLoader.GetComponent<SceneLoader>().sceneToLoad = "Room2";
+        sceneLoader.GetComponent<SceneLoader>().StartLoad();
     }
 
-    public void CloseHelpMenu()
+    public void LoadEnding()
+    {
+        sceneLoader.GetComponent<SceneLoader>().sceneToLoad = "Ending";
+        sceneLoader.GetComponent<SceneLoader>().StartLoad();
+    }
+
+    public void OpenMainMenu()
     {
         MainMenuPage.SetActive(true);
         HowToPlayMenuPage.SetActive(false);
+        ChooseStartPage.SetActive(false);
+        ChooseLevelPage.SetActive(false);
+    }
+
+    public void OpenHelpMenu()
+    {       
+        HowToPlayMenuPage.SetActive(true);
+        MainMenuPage.SetActive(false);
+        ChooseStartPage.SetActive(false);
+        ChooseLevelPage.SetActive(false);
+    }
+
+    public void OpenChooseStartMenu()
+    {
+        ChooseStartPage.SetActive(true);
+        MainMenuPage.SetActive(false);
+        HowToPlayMenuPage.SetActive(false);       
+        ChooseLevelPage.SetActive(false);
+    }
+
+    public void OpenChooseLevelMenu()
+    {
+        ChooseLevelPage.SetActive(true);
+        ChooseStartPage.SetActive(false);
+        MainMenuPage.SetActive(false);
+        HowToPlayMenuPage.SetActive(false);       
     }
 
     public void Quit()
